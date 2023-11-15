726dcbe6c0668990b791c6b2d93af4e9be9c337c
Ann2123 <fortisregin@agmail.com>
2023-11-15 12:36:49 -0400

    nuevo contenido

---
+++ scrip.js
@@ -0,0 +1,36 @@
+
+axios.get('https://leonardoapi.onrender.com/music')
+.then( (res) => { //aqui se reciben los datos del servidor
+
+    let track_list = document.querySelector('#track_list')
+    console.log(track_list)
+    
+    
+    res.data.map ( (song) => { // se mapean las canciones
+
+        // creo el componente
+        let div = document.createElement ('div')
+        div.classList.add('musica')
+
+        //agrego el contenido al componente
+        div.innerHTML =
+        `
+        <img src="${song.path.front}" alt="">
+
+        <li class="cancion">
+                <div class="cancion">
+                     <h3>${song.titlle}</h3>
+                     <p>${song.author}</p>
+                    
+                </div>
+        </li>
+        `
+        //agrego el componente al contenedor
+        track_list.appendChild(div)
+
+    })
+    
+})
+
+
+
